eb95f6b19ca6e01008f4e740a4d147d6de9bef66
hitesh <hitesht9974@gmail.com>
2025-08-07 15:38:22 +0530

    hitesh

--- main.java
+++ main.java
@@ -1,14 +1,17 @@
 import java.util.*;
-
-class Main {
-    public static void main(String[] args) {
+class Main{
+    public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         int b = s.nextInt();
+<<<<<<< HEAD
         System.out.println(a + b);
 
         System.out.println("hello");
 
         System.out.print(a-b);
+=======
+        System.out.println(a+b);
+>>>>>>> parent of 3998513 (added)
     }
 }   
