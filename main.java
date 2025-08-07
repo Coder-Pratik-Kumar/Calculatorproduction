5e65bd3e9f070484f48db0071b4f3e187fe89e2d
hitesh <hitesht9974@gmail.com>
2025-08-07 15:47:41 +0530

    hitesh

--- main.java
+++ main.java
@@ -4,25 +4,24 @@ class Main{
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         int b = s.nextInt();
-<<<<<<< HEAD
+
         System.out.println(a + b);
         System.out.println("hello");
-<<<<<<< HEAD
 
         System.out.print(a-b);
-=======
+
         System.out.println(a+b);
->>>>>>> parent of 3998513 (added)
-=======
+
         System.out.println("partik");
 
-        public void divide (int a , int b){
+        public void divide (int a,int b){
+        System.out.println(a+b);
 
-        }
     }
     
     public void Subtract(int a, int b){
         System.out.print("Hitesh man ho tho kar do");
->>>>>>> 7fd13475ce413d6e6a3a9b7e56a8cfedddce585e
+
     }
-}   
+} 
+}  
