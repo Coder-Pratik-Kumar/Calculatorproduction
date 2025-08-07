dfe3487ff0c2204593a48a3a7836473b52b940f3
Pratik Sharma <149568080+Coder-Pratik-Kumar@users.noreply.github.com>
2025-08-07 14:45:05 +0530

    pratik added

--- main.java
+++ main.java
@@ -4,6 +4,8 @@ class Main{
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         int b = s.nextInt();
-        System.out.println(a+b);
+        System.out.println(a + b);
+        
+        System.out.println("partik");
     }
 }
