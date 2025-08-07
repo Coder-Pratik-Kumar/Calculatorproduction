39985135c84cc82dc3559026a52da86ffa644384
Pratik Sharma <149568080+Coder-Pratik-Kumar@users.noreply.github.com>
2025-08-07 14:42:00 +0530

    added

--- main.java
+++ main.java
@@ -1,9 +1,12 @@
 import java.util.*;
-class Main{
-    public static void main(String[] args){
+
+class Main {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         int b = s.nextInt();
-        System.out.println(a+b);
+        System.out.println(a + b);
+
+        System.out.println("hello");
     }
 }
